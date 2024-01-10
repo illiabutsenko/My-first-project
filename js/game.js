1b7a41e46404d369b774a44913a2bb763244059d
illiabutsenko <ilya1990.0826@gmail.com>
2024-01-10 18:45:08 +0100

    adding key controls

--- js/game.js
+++ js/game.js
@@ -9,13 +9,36 @@ plane.src = "img/plane2.png";
 bg.src = "img/bg.png";
 pipe.src = "img/pipe.png";
 
+addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "ArrowDown":
+      yPlane += 50;
+      break;
+  }
+});
+window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "ArrowUp":
+      yPlane -= 50;
+      break;
+  }
+});
+
+var xPlane = 10;
+var yPlane = 220;
+var xBg = 10;
+var fly = 0;
+
 function draw() {
-  ctx.drawImage(bg, 0, 0);
-  
+  ctx.drawImage(bg, xBg, 0);
+
   ctx.drawImage(pipe, 220, -170);
   ctx.drawImage(pipe, 220, 350);
-  
-  ctx.drawImage(plane, 10, 220)
+
+  ctx.drawImage(plane, xPlane, yPlane, 100, 100);
+
+  xBg -= fly;
+  requestAnimationFrame(draw);
 }
 
 pipe.onload = draw;
