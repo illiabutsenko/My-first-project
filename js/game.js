fd19307f37fed22e3dd9584ba1c895439940d7e7
illiabutsenko <ilya1990.0826@gmail.com>
2024-01-11 22:01:56 +0100

    add score, audio

--- js/game.js
+++ js/game.js
@@ -1,5 +1,5 @@
-var cvs = document.getElementById("canvas");
-var ctx = cvs.getContext("2d");
+let cvs = document.getElementById("canvas");
+let ctx = cvs.getContext("2d");
 
 var plane = new Image();
 var bg = new Image();
@@ -8,6 +8,9 @@ var pipe = new Image();
 plane.src = "img/plane2.png";
 bg.src = "img/bg.png";
 pipe.src = "img/pipe.png";
+
+var score_audio = new Audio();
+score_audio.src = "audio/score.mp3";
 
 addEventListener("keydown", (e) => {
   switch (e.key) {
@@ -40,6 +43,7 @@ var xPlane = 10;
 var yPlane = 220;
 var planeWidth = 70;
 var planeHeight = 70;
+var score = 0;
 
 function draw() {
   for (let i = 0; i < bgAr.length; i++) {
@@ -79,12 +83,19 @@ function draw() {
     ) {
       location.reload();
     }
+    if (pipeAr[i].x == 5) {
+      score++;
+      score_audio.play();
+    }
   }
 
   ctx.drawImage(plane, xPlane, yPlane, planeWidth, planeHeight);
+  ctx.fillStyle = "#00008B";
+  ctx.font = "24px Verdana";
+  ctx.fillText(`Score: ${score}`, 10, 490);
   requestAnimationFrame(draw);
 }
 
-console.log(pipe.height, pipe.width);
+// console.log(pipe.height, pipe.width);
 
 pipe.onload = draw;
